f78836e1236bca43a7ef6d94da8d4ae1811fbdb2
Vasireddy, Sahithi <svasire@iu.edu>
2022-11-26 14:02:24 -0500

    forgot password draft

--- src/App.js
+++ src/App.js
@@ -1,74 +1,26 @@
-import React, { useRef, useEffect } from 'react';
-import { useLocation,Routes } from 'react-router-dom';
-import AppRoute from './utils/AppRoute';
-import ScrollReveal from './utils/ScrollReveal';
-import { Route } from 'react-router-dom';
+import React from "react";
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import {Login} from './login';
+import {Signin} from './signin';
+import {Home} from './home';
+import {GoogleSignIn} from './gsignin';
 
-// Layouts
-import LayoutDefault from './layouts/LayoutDefault';
-import LayoutCustom from './layouts/LayoutCustom';
 
-// Views 
-import Home from './views/Home';
-import Login from './views/Login';
-import UserRegistration from './views/UserRegistration';
-import Search from './views/Search';
-import GSignIn from './views/GSignIn';
-import View from './views/View';
-import PetRegistration from './views/PetRegistration';
-import Payment from './views/Payment';
-import Reservations from './views/Reservations';
-import Approval from './views/Approval';
-import Complaints from './views/Complaints';
-import ForgotPassword from "./views/ForgotPassword/index.js";
-import ResetPassword from "./views/ResetPassword/index.js"
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-  background-color: whitesmoke;
-`;
-const App = () => {
-
-  const childRef = useRef();
-  let location = useLocation();
-
-  useEffect(() => {
-    const page = location.pathname;
-    document.body.classList.add('is-loaded')
-    childRef.current.init();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location]);
-
+function App() {
   return (
-    <ScrollReveal
-      ref={childRef}
-      children={() => (
-     
-        <Routes>
-          <Route path="/" element={<AppRoute exact path="/" component={Home} layout={LayoutDefault} />}  />
-          <Route path="/login" element={<AppRoute exact path="/login" component={Login} layout={LayoutCustom} />}  />
-          <Route path="/signin" element={<AppRoute exact path="/signin" component={UserRegistration} layout={LayoutCustom} />}  />
-          <Route path="/gsignin" element={<AppRoute exact path="/gsignin" component={GSignIn} layout={LayoutCustom} />}  />
-          <Route path="/search" element={<AppRoute exact path="/search" component={Search} layout={LayoutCustom} />}  />
-          <Route path="/view" element={<AppRoute exact path="/view" component={View} layout={LayoutCustom} />}  />
-          <Route path="/petRegistration" element={<AppRoute exact path="/petRegistration" component={PetRegistration} layout={LayoutCustom} />}  />
-          <Route path="/reservations" element={<AppRoute exact path="/reservations" component={Reservations} layout={Reservations} />}  />
-          <Route path="/payment" element={<AppRoute exact path="/payment" component={Payment} layout={LayoutCustom} />}  />
-          <Route path="/approvals" element={<AppRoute exact path="/approvals" component={Approval} layout={LayoutCustom} />}  />
-          <Route path="/complaints" element={<AppRoute exact path="/complaints" component={Complaints} layout={LayoutCustom} />}  />
-          <Route path="/forgotpassword" element={<AppRoute exact path="/forgotpassword" component={ForgotPassword} layout={LayoutCustom} />}  />
-          <Route path="/reset" element={<AppRoute exact path="/reset" component={ResetPassword} layout={LayoutCustom} />}  />
-
-        
-          </Routes>
-  
-      )} />
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/gsignin" element={<GoogleSignIn />} />
+      </Routes>
+    </div>
+    
   );
 }
+
+
 
 export default App;
--- src/components/elements/rows.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Row = styled.div`
-    width: 90%;
-    margin-bottom: 5px;
-    margin-top: 5px;
-`;
-
-export default Row;
